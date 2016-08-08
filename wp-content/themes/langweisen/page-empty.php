cd51c965ee118fe7afc3d6e8233b9025a69ffec5
Mike <mike@mamix.ch>
2016-08-08 16:55:10 +0200

    homepage image

---
+++ wp-content/themes/langweisen/page-empty.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Empty Template
+ * Author: raymond@philippinedev.com
+ * Date: 2016-08-08
+ * Time: 22:35
+ */
+?>
+<!doctype html>
+<html>
+<head>
+  <title>See 177</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
+</head>
+<body style="background:#dce2e2;text-align:center">
+    <?php
+    // TO SHOW THE PAGE CONTENTS
+    while ( have_posts() ) : ?> <!--Because the_content() works only inside a WP Loop -->
+      <!--<?php the_post(); ?>-->
+        <div class="entry-content-page">
+            <?php the_content(); ?> <!-- Page Content -->
+        </div><!-- .entry-content-page -->
+
+    <?php
+    endwhile; //resetting the page loop
+    wp_reset_query(); //resetting the page query
+    ?>
+</body>
+</html>
